6c348bf9d6fc37317bb39f87b332f9eb9af07d96
Yask Srivastava <yask123@gmail.com>
2015-10-13 23:18:32 +0530

    initial

---
+++ Formula/instantmusic.rb
@@ -0,0 +1,25 @@
+class Gitignore < Formula
+  homepage "https://github.com/yask123/Instant-Music-Downloader"
+  url "https://pypi.python.org/pypi/instantmusic/1.8"
+  md5 "2b1fc7fe164f147b071138cb07952e75"
+
+  def install
+    ENV["PYTHONPATH"] = libexec/"vendor/lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+
+    %w[docopt].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  test do
+    
+  end
+end
